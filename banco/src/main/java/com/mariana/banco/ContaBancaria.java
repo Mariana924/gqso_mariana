5499f56805fe074941e2f26305cc1ac916c154f6
Mariana924 <mas11@aluno.ifal.edu.br>
2021-04-30 21:45:08 -0300

    Criei classe ContaBancaria com atributo saldo = 0

--- banco/src/main/java/com/mariana/banco/ContaBancaria.java
+++ banco/src/main/java/com/mariana/banco/ContaBancaria.java
@@ -2,4 +2,14 @@ package com.mariana.banco;
 
 public class ContaBancaria {
     
+    private double saldo = 0;
+    
+
+    public double saldo() {
+        return this.saldo;
+    }
+
+    private void setSaldo(double v) {
+        this.saldo = v;
+    }
 }
